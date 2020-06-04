f9598b76e457f5bd37d7c6ba865fdeb9d7bf6d3c
ksoon1985 <ksoon1985@naver.com>
2020-06-04 21:17:41 +0900

    controllerAction 주석

--- jspEx/src/model2/board/ControllerAction.java
+++ jspEx/src/model2/board/ControllerAction.java
@@ -23,27 +23,41 @@ import model2.board.action.CommandAction;
 public class ControllerAction extends HttpServlet {
    
    private static final long serialVersionUID = 1L;
+   
    //명령과 클래스가 들어가있는 Map 선언 HashMap();
    private HashMap<String, Object> commandMap = new HashMap<String,Object>();
      
-   //서블릿(Servlet)이 실행되기 전에 초기화 과정
+   /* 
+     	서블릿(Servlet)이 실행되기 전에 초기화 과정(단 한번 만 실행)
+   		ServletConfig는 서블릿당 하나가 생성되며 web.xml에서 우리가 필요에 따라 입력한 파라미터 값들을 담고 있음
+   		ServletConfig 로 서블릿을 초기화 시키면 유지보수 성이 높아짐 
+   */ 
    public void init(ServletConfig config) {
        //필요한 Action--비지니스로직이 있음 미리 올려놓고 시작
        //웹 메모리에 올리면서 서버가 실행
 //      System.out.println(config);
 	   
+	  // web.xml에서 init-param param-name 이 configProperty인 값을 가져옴  
+	  // configProperty 엔 C:/study/workspace/jspEx/src/model2/board/Command.properties이 들어있음 
       String props = config.getInitParameter("configProperty");
-//      String props = "Command.properties";
      
+      /*
+       		Properties 클래스
+       		- Map 계열의 컬렉션 프레임워크와 비슷하게 동작
+       		- Key = Value 형태로 된  파일이름.properties 파일 또는 xml 
+       		- key 를 주면 value를 반환하는 기능을 가짐
+       		- DB의 연결정보 등을 저장해두는 용도로 사용 
+       */
       Properties pr = new Properties();
       
       FileInputStream f = null;
       try {
-         //파일로 오픈
+         //Command.properties 파일 연결 
          f = new FileInputStream(props);
          //그 파일은 특별히 프로퍼티스 임 (=을 기준으로 왼쪽은 키, 오른쪽은 값)
+         
          pr.load(f);
-         //프로퍼티를 읽어서
+         //스트림으로 열린 properties파일 객체(Command.properties)를 로드함 
          //키를 명령에 저장하고, value 있는것을 파일로 오픈해서 읽게 하기
          //해당되는 클래스는 공통에 메소드가 있어서 그걸 처리하는 명령
       }catch(IOException e) {
@@ -84,7 +98,7 @@ public class ControllerAction extends HttpServlet {
          }
       }
       
-   }
+   }//init
    
     /**
      * @see HttpServlet#HttpServlet()
